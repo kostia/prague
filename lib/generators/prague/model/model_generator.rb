7e63bc1338c9988f421cea04103e842835fb0772
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2012-09-10 15:17:27 +0200

    default model name fixed in model generator

--- lib/generators/prague/model/model_generator.rb
+++ lib/generators/prague/model/model_generator.rb
@@ -4,7 +4,7 @@ module Prague
   module Generators
     class ModelGenerator < ::ActiveRecord::Generators::Base
       desc 'This generator generates a model for pages'
-      argument :name, default: 'Page'
+      argument :name, default: 'page'
       source_root File.expand_path('../templates', __FILE__)
 
       def create_migration
@@ -12,7 +12,7 @@ module Prague
       end
 
       def create_model
-        template 'model.rb.erb', "app/models/#{name}.rb"
+        template 'model.rb.erb', "app/models/#{singular_name}.rb"
       end
     end
   end
