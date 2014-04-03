ebcb260888a89365ce1498269f376ca4293f015b
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-04-03 22:55:46 +0200

    Removed outdated dummy test app

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '9c60b3d0ea803bbe6904f8537c64ea53ce3a9914e254e70487b93a68d808f57d9dd2b212d4c71a912aab2c9a9d05f1f0c9fde20cc39e8736db04a8c22bbacb2f'
--- test/prague_test.rb
+++ test/prague_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PragueTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Prague
+  def test_app_dir
+    File.expand_path '../dummy', __FILE__
+  end
+
+  def in_test_app
+    Dir.chdir test_app_dir { yield }
+  end
+
+  def generate(args)
+    in_test_app { sh "bundle exec rails g prague:#{args}" }
+  end
+
+  def generate_scaffold(name = nil)
+    generate "scaffold #{name}"
+  end
+
+  test 'can call generator' do
+    generate_scaffold
   end
 end
